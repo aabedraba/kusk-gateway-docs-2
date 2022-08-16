d1e4b34cd047ae83cb5eb14775f923e986c0f92a
Abdallah Abedraba <aabedraba@gmail.com>
2022-08-16 15:07:13 +0200

    move everything to docs folder

---
+++ docs/build/assets/js/403cbd93.93dc0255.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkusk_gateway_docs_2=self.webpackChunkkusk_gateway_docs_2||[]).push([[128],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ docs/build/assets/js/c4f5d8e4.d77b6a1a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkusk_gateway_docs_2=self.webpackChunkkusk_gateway_docs_2||[]).push([[195],{2841:(e,s,t)=>{t.r(s),t.d(s,{default:()=>k});var a=t(7294),c=t(6775);const k=()=>a.createElement(c.l_,{to:"/docs/intro"})}}]);
