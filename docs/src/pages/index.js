f5d43d60c14c29e9235a164a4e7b0a46bd8b08c9
Abdallah Abedraba <aabedraba@gmail.com>
2022-08-17 10:46:12 +0200

    change links to source directory

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 const Home = () => {
-  return <Redirect to="/kusk-gateway-docs-2/docs/intro" />;
+  return <Redirect to="/intro" />;
 };
 
 export default Home;
